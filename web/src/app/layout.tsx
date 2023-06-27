cffa921505b2129f962b0519dd08513a3b9715f2
Fanuel Couto <fanuel.couto99@gmail.com>
2023-06-26 21:14:08 -0300

    Configurando estrutura inicial do projeto

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Pomodoro',
+  description: 'Aplicação para controle de tempo usando a técnica pomodoro',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
