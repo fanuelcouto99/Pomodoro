39bef9622e6c0ad1c84a395244423e5fabfd81f8
Fanuel Couto <fanuel.couto99@gmail.com>
2023-06-30 12:09:34 -0300

    STYLE[Layout]: Finaliado estilização do header da aplicação

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,9 +1,31 @@
+'use client'
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../assets/logo.svg';
 import { Timer, ScrollText } from 'lucide-react';
 
 export function Header() {
+    const [isHomeActive, setIsHomeActive] = useState(false);
+    const [isHistoryActive, setIsHistoryActive] = useState(false);
+
+    function pageIsActive(page: string) {
+        switch (page) {
+            case 'home': 
+                setIsHomeActive(true);
+                setIsHistoryActive(false);
+            break;
+            
+            case 'history' :
+                setIsHistoryActive(true);
+                setIsHomeActive(false);
+            break;
+            
+            default:
+                console.log('Página não encontrada!');
+        };
+    };
+
     return (
         <div className="flex items-center justify-between">
             <Image src={logo} alt='' />
@@ -12,15 +34,17 @@ export function Header() {
                 <Link
                     href="/"
                     title='Timer'
-                    className='w-12 h-12 flex items-center justify-center border-t-[3px] border-b-[3px] border-t-transparent border-b-transparent hover:border-b-green-500 transition-colors group'>
-                    <Timer className='stroke-gray-100 group-active:stroke-green-500' />
+                    onClick={() => pageIsActive('home')}
+                    className='w-12 h-12 flex items-center justify-center border-t-[3px] border-b-[3px] border-t-transparent border-b-transparent hover:border-b-green-500 transition-colors'>
+                    <Timer className={`stroke-gray-100 ${isHomeActive && 'stroke-green-500'}`} />
                 </Link>
 
                 <Link
                     href="/history"
                     title='Histórico'
-                    className='w-12 h-12 flex items-center justify-center border-t-[3px] border-b-[3px] border-t-transparent border-b-transparent hover:border-b-green-500 transition-colors group'>
-                    <ScrollText className='stroke-gray-100 group-active:stroke-green-500' />
+                    onClick={() => pageIsActive('history')}
+                    className='w-12 h-12 flex items-center justify-center border-t-[3px] border-b-[3px] border-t-transparent border-b-transparent hover:border-b-green-500 transition-colors'>
+                    <ScrollText className={`stroke-gray-100 ${isHistoryActive && 'stroke-green-500'}`} />
                 </Link>
             </nav>
         </div>
