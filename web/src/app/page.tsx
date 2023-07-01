136deea659b906d31928d3855ae7afb230e64592
Fanuel Couto <fanuel.couto99@gmail.com>
2023-07-01 13:03:32 -0300

    STYLE[Home]: Finalizado estilização do form

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,17 +7,29 @@ export default function Home() {
         <div className='w-full flex items-center justify-center gap-2 text-gray-100 text-lg font-bold flex-wrap'>
           <label htmlFor='task'>Vou trabalhar em</label>
           <input
-            className=''
+            className='inputTask bg-transparent h-10 border-0 border-b-2 border-b-gray-500 font-bold text-lg px-2 text-gray-100 placeholder:text-gray-500 placeholder:text-base flex-1 focus:shadow-none focus:border-green-500 focus:ring-0'
             type='text'
             id="task"
+            placeholder='Dê um nome para o seu projeto'
+            list='taskSuggestions'
           />
+
+          <datalist id='taskSuggestions'>
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <input
-            className=''
+            className='bg-transparent h-10 border-0 border-b-2 border-b-gray-500 font-bold text-lg px-2 text-gray-100 placeholder:text-gray-500 placeholder:text-base w-16 focus:shadow-none focus:border-green-500 focus:ring-0'
             type="number"
             name="minutesAmount"
             id="minutesAmount"
+            placeholder='00'
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos</span>
