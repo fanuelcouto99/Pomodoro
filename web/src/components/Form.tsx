eacc0d00ec7ab172f3af4fd7b3cc119bad64c54c
Fanuel Couto <fanuel.couto99@gmail.com>
2023-07-05 21:09:44 -0300

    FEAT[Countdown]: Criado função para interromper ciclo de tarefa, e também zerar o contador após encerrar o ciclo

--- web/src/components/Form.tsx
+++ web/src/components/Form.tsx
@@ -5,11 +5,12 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
 import { differenceInSeconds } from 'date-fns';
 import { Play } from 'lucide-react';
+import { Hand } from 'lucide-react';
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa!"),
     minutesAmount: zod.number()
-        .min(5, "O ciclo precisa ser de no mínimo de 5 minutos!")
+        .min(1, "O ciclo precisa ser de no mínimo de 5 minutos!")
         .max(60, "O ciclo precisa ser de no máximo de 60 minutos!")
 });
 
@@ -20,6 +21,8 @@ interface CycleProps {
     task: string;
     minutesAmount: number;
     startDate: Date;
+    interruptedDate?: Date;
+    finishedDate?: Date;
 };
 
 export function Form() {
@@ -36,20 +39,36 @@ export function Form() {
     });
 
     const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
     useEffect(() => {
         let interval: number;
 
         if (activeCycle) {
             interval = Number(setInterval(() => {
-                setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate));
+                const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate);
+
+                if(secondsDifference >= totalSeconds) {
+                    setCycles((state) => state.map(cycle => {
+                        if(cycle.id === activeCycleId) {
+                            return {...cycle, finishedDateDate: new Date()}
+                        } else {
+                            return cycle
+                        }
+                    }));
+                    setAmountSecondsPassed(totalSeconds);
+                    clearInterval(interval);
+                    setActiveCycleId(null);
+                } else {
+                    setAmountSecondsPassed(secondsDifference);
+                }
             }, 1000));
         }
 
         return () => {
             clearInterval(interval);
         };
-    }, [activeCycle]);
+    }, [activeCycle, totalSeconds, activeCycleId]);
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         const id = String(new Date().getTime());
@@ -66,7 +85,17 @@ export function Form() {
         reset();
     };
 
-    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+    function handleInterruptCycle() {
+        setCycles((state) => state.map(cycle => {
+            if(cycle.id === activeCycleId) {
+                return {...cycle, interruptedDate: new Date()}
+            } else {
+                return cycle
+            }
+        }));
+        setActiveCycleId(null);
+    };
+
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
     const minutesAmount = Math.floor(currentSeconds / 60);
     const secondsAmount = currentSeconds % 60;
@@ -88,11 +117,12 @@ export function Form() {
                 <div className='w-full flex items-center justify-center gap-2 text-gray-100 text-lg font-bold flex-wrap'>
                     <label htmlFor='task'>Vou trabalhar em</label>
                     <input
-                        className='inputTask bg-transparent h-10 border-0 border-b-2 border-b-gray-500 font-bold text-lg px-2 text-gray-100 placeholder:text-gray-500 placeholder:text-base flex-1 focus:shadow-none focus:border-green-500 focus:ring-0'
+                        className='inputTask bg-transparent h-10 border-0 border-b-2 border-b-gray-500 font-bold text-lg px-2 text-gray-100 placeholder:text-gray-500 placeholder:text-base flex-1 focus:shadow-none focus:border-green-500 focus:ring-0 disabled:cursor-not-allowed disabled:opacity-70'
                         type='text'
                         id="task"
                         placeholder='Dê um nome para o seu projeto'
                         list='taskSuggestions'
+                        disabled={!!activeCycle}
                         {...register('task')}
                     />
 
@@ -104,13 +134,14 @@ export function Form() {
 
                     <label htmlFor="minutesAmount">durante</label>
                     <input
-                        className='bg-transparent h-10 border-0 border-b-2 border-b-gray-500 font-bold text-lg px-2 text-gray-100 placeholder:text-gray-500 placeholder:text-base w-16 focus:shadow-none focus:border-green-500 focus:ring-0'
+                        className='bg-transparent h-10 border-0 border-b-2 border-b-gray-500 font-bold text-lg px-2 text-gray-100 placeholder:text-gray-500 placeholder:text-base w-16 focus:shadow-none focus:border-green-500 focus:ring-0 disabled:cursor-not-allowed disabled:opacity-70'
                         type="number"
                         id="minutesAmount"
                         placeholder='00'
                         step={5}
-                        min={5}
+                        min={1}
                         max={60}
+                        disabled={!!activeCycle}
                         {...register('minutesAmount', { valueAsNumber: true })}
                     />
 
@@ -125,10 +156,17 @@ export function Form() {
                     <span className='bg-gray-700 py-8 px-4 rounded-lg'>{seconds[1]}</span>
                 </div>
 
-                <button type="submit" disabled={isSubmitDisabled} className='w-full p-4 rounded-lg flex items-center justify-center gap-2 font-bold cursor-pointer bg-green-500 text-gray-100 enabled:hover:bg-green-700 disabled:opacity-70 disabled:cursor-not-allowed'>
-                    <Play />
-                    Começar
-                </button>
+                {activeCycle ? (
+                    <button type="button" onClick={handleInterruptCycle} className='w-full p-4 rounded-lg flex items-center justify-center gap-2 font-bold cursor-pointer bg-red-500 text-gray-100 enabled:hover:bg-red-700 disabled:opacity-70 disabled:cursor-not-allowed'>
+                        <Hand />
+                        Interromper
+                    </button>
+                ) : (
+                    <button type="submit" disabled={isSubmitDisabled} className='w-full p-4 rounded-lg flex items-center justify-center gap-2 font-bold cursor-pointer bg-green-500 text-gray-100 enabled:hover:bg-green-700 disabled:opacity-70 disabled:cursor-not-allowed'>
+                        <Play />
+                        Começar
+                    </button>
+                )}
             </form>
         </>
     );
